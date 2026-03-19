d035cdf861da8f61b22fcbae5c11d0999dc0b00d
jheremy99 <999935626g@gmail.com>
2026-03-19 18:18:34 -0500

    primer commit agregados

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/dalab-studio-site/", // 👈 CLAVE
+
   server: {
     host: "::",
     port: 8080,
